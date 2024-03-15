97510d5cdb482a84402d5f335da4db8bb768336d
Andrey Adriano <dey2010dey@gmail.com>
2024-03-15 18:46:13 -0300

    send Hello through sockets

--- server/index.js
+++ server/index.js
@@ -10,9 +10,17 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
     cors: {
-        origin: "http://localhost:3000",
+        origin: "http://localhost:5173",
         methods: ["GET", "POST"]
     },
+});
+
+io.on("connection", (socket) => {
+    console.log('User connected: ' + socket.id);
+
+    socket.on("send_message", (data) => {
+        socket.broadcast.emit("receive_message", data);
+    })
 });
 
 server.listen(3001, () => {
