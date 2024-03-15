f7afa81a477667d6939b943c84140d1587d73503
Andrey Adriano <dey2010dey@gmail.com>
2024-03-15 18:12:05 -0300

    fix project

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,19 @@
 import './App.css'
+import io from 'socket.io-client'
+
+const socket = io.connect("http://localhost:3001")
 
 function App() {
+
+  const sendMessage = () => {
+    socket.emit()
+  }
 
   return (
     <>
       <div>
-
+        <input placeholder="Message" />
+        <button onClick={sendMessage}>Send message</button>
       </div>
     </>
   )
